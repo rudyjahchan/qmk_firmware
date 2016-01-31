41c0c58323e8cda8c1c12985fecab41e4519ef8a
Rudy Jahchan <rudy@carbonfive.com>
2016-01-31 15:12:05 -0800

    Simplify

--- keyboard/ergodox_ez/keymaps/planckd/keymap.c
+++ keyboard/ergodox_ez/keymaps/planckd/keymap.c
@@ -13,13 +13,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |  GRV   |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |  BkSp  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |  BkSp  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Esc    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L3  | Ctrl | Alt  | LGui | ~L2  |                                       | ~L1  | Left | Down |  Up  |Right |
+ *   |  L3  | Ctrl | Alt  | LGui |  L2  |                                       |  L1  | Left | Down |  Up  |Right |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -33,30 +33,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,         KC_1,         KC_2,    KC_3,    KC_4,   KC_5,    KC_NO,
-        KC_TAB,         KC_Q,         KC_W,    KC_E,    KC_R,   KC_T,    KC_NO,
-        KC_ESC,         KC_A,         KC_S,    KC_D,    KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,    KC_C,    KC_V,   KC_B,    ALL_T(KC_NO),
-        LT(MDIA,KC_NO), KC_LCTL,      KC_LALT, KC_LGUI, LT(LOWR, KC_NO),
-                                               ALT_T(KC_APP),   KC_LGUI,
-                                                                KC_HOME,
-                                               KC_SPC,  KC_TAB, KC_END,
+        KC_GRV,   KC_1,         KC_2,    KC_3,    KC_4,     KC_5,   KC_NO,
+        KC_TAB,   KC_Q,         KC_W,    KC_E,    KC_R,     KC_T,   KC_NO,
+        KC_ESC,   KC_A,         KC_S,    KC_D,    KC_F,     KC_G,
+        KC_LSFT,  CTL_T(KC_Z),  KC_X,    KC_C,    KC_V,     KC_B,   ALL_T(KC_NO),
+        MO(MDIA), KC_LCTL,      KC_LALT, KC_LGUI, MO(LOWR),
+                                                  ALT_T(KC_APP),   KC_LGUI,
+                                                                   KC_HOME,
+                                                  KC_SPC,  KC_TAB, KC_END,
         // right hand
-             KC_NO,        KC_6,   KC_7,   KC_8,    KC_9,    KC_0,              KC_BSPC,
-             KC_NO,        KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,              KC_BSLS,
-                           KC_H,   KC_J,   KC_K,    KC_L,    LT(MDIA, KC_SCLN), KC_QUOT,
-             MEH_T(KC_NO), KC_N,   KC_M,   KC_COMM, KC_DOT,  CTL_T(KC_SLSH),    KC_RSFT,
-                        LT(RISE, KC_NO),   KC_LEFT, KC_DOWN, KC_UP,             KC_RGHT,
-             KC_RALT,      CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN,     KC_ESC, KC_ENT
+        KC_NO,        KC_6,   KC_7,     KC_8,    KC_9,    KC_0,              KC_BSPC,
+        KC_NO,        KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,              KC_BSPC,
+                      KC_H,   KC_J,     KC_K,    KC_L,    LT(MDIA, KC_SCLN), KC_QUOT,
+        MEH_T(KC_NO), KC_N,   KC_M,     KC_COMM, KC_DOT,  CTL_T(KC_SLSH),    KC_RSFT,
+                              MO(RISE), KC_LEFT, KC_DOWN, KC_UP,             KC_RGHT,
+        KC_RALT,      CTL_T(KC_ESC),
+        KC_PGUP,
+        KC_PGDN,     KC_ESC, KC_ENT
     ),
 /* Keymap 1: Raised Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |   `    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   F12  |
+ * |   `    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |------|           |------|      |   -  |   =  |   [  |   ]  |    \   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Raised
 [RISE] = KEYMAP(
        // left hand
-       KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_GRV , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_F11 , KC_F12 , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -84,8 +84,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     KC_TRNS,
                                   KC_TRNS, KC_TRNS, KC_TRNS,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
                 KC_TRNS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -217,6 +217,9 @@ void * matrix_scan_user(void) {
             break;
         case 2:
             ergodox_right_led_2_on();
+            break;
+        case 3:
+            ergodox_right_led_3_on();
             break;
         default:
             // none
